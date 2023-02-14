db9f5e136adb2b24fee3603cc75498e42af198d7
7jjin <wlsgud6305@naver.com>
2023-02-14 15:59:02 +0900

    02/14

---
+++ assets/assets/js/questions.js
@@ -0,0 +1,14 @@
+import {questions}from './data.js'
+
+const progerssValueEl = document.querySelector(".progress .vulue");
+const numberEl = document.querySelector(".number");
+const questionEl = document.querySelector(".question");
+const choice1El = document.querySelector(".choice1");
+const choice2El = document.querySelector(".choice2");
+
+let currentNumber = 0;
+let mbti = '';
+
+function renderQuestion(){
+    const 
+}
