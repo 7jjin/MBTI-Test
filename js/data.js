d094a44f48ad6646d3cc153d57ff5d665b1ed100
7jjin <wlsgud6305@naver.com>
2023-02-15 16:52:07 +0900

    수정(2)

--- js/data.js
+++ js/data.js
@@ -83,7 +83,7 @@ export const questions = [
 export const results = [
   {
     title: '출근 시간은<br>내가 정해!',
-    character: '/assets/assets/images/result_character1.png',
+    character: '/images/result_character1.png',
     results: [
       '혼자서 멘탈 케어가 가능해요!<br>소속이 없어도, 벌이가 불안정해도 저는 지금이 좋아요!',
       '일에 대한 욕심이 많아요.<br>행복한 야근이라고 할 수 있어요!',
@@ -91,12 +91,12 @@ export const results = [
       '리더십 있다는 소리를 자주 들어요.<br>추진력이 있는 편이죠~ (뿌듯)'
     ],
     jobs: ['프리랜서', '창업가'],
-    lectureImg: '/assets/assets/images/result_lecture1.png',
+    lectureImg: '/images/result_lecture1.png',
     lectureUrl: 'https://bit.ly/3Wr0kt6'
   },
   {
     title: '프로직진러!<br>진행력 갑',
-    character: '/assets/assets/images/result_character2.png',
+    character: '/images/result_character2.png',
     results: [
       '나는야 회사 사람들 멘탈 지킴이!<br>언제나 토닥토닥 으샤으샤 응원하며 다니는 파워 인싸!',
       '여러 사람과 협업이 필요한 직무에 찰떡!',
@@ -104,12 +104,12 @@ export const results = [
       '회사 행사나 크리스마스 이벤트,<br>회사사람들 생일까지 우리가 다 챙길게요'
     ],
     jobs: ['마케터', '기획자'],
-    lectureImg: '/assets/assets/images/result_lecture2.png',
+    lectureImg: '/images/result_lecture2.png',
     lectureUrl: 'https://bit.ly/3SZl1t9'
   },
   {
     title: '우리 무기는<br>상상력!',
-    character: '/assets/assets/images/result_character3.png',
+    character: '/images/result_character3.png',
     results: [
       '이것저것 관심이 많은 편이에요.<br>모든 게 궁금해요!',
       '섬세함이 장점으로<br>업무를 센스 있게 처리한다는 말을 자주 들어요.',
@@ -117,12 +117,12 @@ export const results = [
       '업무에 예민할 때도 있지만,<br>그래서 결과물은 완벽하죠!'
     ],
     jobs: ['디자이너', '예술가'],
-    lectureImg: '/assets/assets/images/result_lecture3.png',
+    lectureImg: '/images/result_lecture3.png',
     lectureUrl: 'https://bit.ly/3DWlesG'
   },
   {
     title: '인생은<br>한방!',
-    character: '/assets/assets/images/result_character4.png',
+    character: '/images/result_character4.png',
     results: [
       '전략적으로 계획을 세우는 걸 좋아해요!',
       '평소 경제와 사회 이슈에 관심이 매우 많아요.',
@@ -130,12 +130,12 @@ export const results = [
       '일할 땐 일하고! 놀 땐 놀고!<br>뭐든 확실한 게 좋아요.'
     ],
     jobs: ['부동산<br>투자자', '주식<br>투자자'],
-    lectureImg: '/assets/assets/images/result_lecture4.png',
+    lectureImg: '/images/result_lecture4.png',
     lectureUrl: 'https://bit.ly/3fvoTo5'
   },
   {
     title: '한 번 집중하면<br>멈출 수 없지!',
-    character: '/assets/assets/images/result_character5.png',
+    character: '/images/result_character5.png',
     results: [
       '저는 위선과 가식이 없어요.<br>진실만 말하죠. (진지)',
       '혼자서 더 잘해요.<br>솔로 워커 최고!',
@@ -143,12 +143,12 @@ export const results = [
       '(이상) <<<<<< (현실)<br>가끔 이성에게 상처를 줄 때가 있어요, 그게 아닌데...'
     ],
     jobs: ['개발자', '데이터분석가'],
-    lectureImg: '/assets/assets/btn-green/images/result_lecture5.png',
+    lectureImg: '/btn-green/images/result_lecture5.png',
     lectureUrl: 'https://bit.ly/3U0O3Kj'
   },
   {
     title: '인생 욜로지~<br>일단 놀자 ><',
-    character: '/assets/assets/images/result_character6.png',
+    character: '/images/result_character6.png',
     results: [
       '취미가 100개!<br>이것저것 다재다능해요~',
       '한가하냐고요?<br>원래 백수가 젤 바쁜 거에요!',
@@ -156,7 +156,7 @@ export const results = [
       '가끔이지만, 갓생살기 도전해요!'
     ],
     jobs: ['돈 많은 백수', '유튜버'],
-    lectureImg: '/assets/assets/images/result_lecture6.png',
+    lectureImg: '/images/result_lecture6.png',
     lectureUrl: 'https://bit.ly/3fzdinR'
   }
 ]
